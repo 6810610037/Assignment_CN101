794300660bbc39af50641cb78d330ef000246398
Komol Bunrueang <6810610037@cn102>
2025-10-18 17:25:59 +0700

    w

--- word_freq.py
+++ word_freq.py
@@ -5,5 +5,14 @@ while True:
     word = input("Enter word (END to stop):")
     if word == "END":
         break
-    for word in not list1:
+    if word not in list1:
         list1.append(word)
+        count_list.append(1)
+    for i in range(len(list1) - 1):
+        if word == list1[i]:
+            count_list[i] += 1
+highest_frequency_index = count_list.index(max(count_list))
+
+print(f"Words : {list1}")
+print(f"Counts: {count_list}")
+print(f"Most frequents: '{list1[highest_frequency_index]}' ({max(count_list)})")
